0b51b41eb61a2ee3ded0795241037594ca7d87a7
Jeetjyoti Deka <jeetjyoti2020@gmail.com>
2024-01-09 21:44:36 +0530

    Fix Images

--- components/shared/NavMenu.tsx
+++ components/shared/NavMenu.tsx
@@ -2,53 +2,7 @@
 
 import { useState } from "react";
 import NavMenuItem from "./NavMenuItem";
-
-const NAV_MENU_ITEMS = [
-  {
-    label: "Home",
-    icon: "./icons/vector-1.svg",
-  },
-  {
-    label: "Orders",
-    icon: "./icons/vector-2.svg",
-  },
-  {
-    label: "Products",
-    icon: "./icons/vector-3.svg",
-  },
-  {
-    label: "Delivery",
-    icon: "./icons/vector-4.svg",
-  },
-  {
-    label: "Marketing",
-    icon: "./icons/vector-5.svg",
-  },
-  {
-    label: "Analytics",
-    icon: "./icons/vector-6.svg",
-  },
-  {
-    label: "Payouts",
-    icon: "./icons/vector-7.svg",
-  },
-  {
-    label: "Discounts",
-    icon: "./icons/vector-8.svg",
-  },
-  {
-    label: "Audience",
-    icon: "./icons/vector-9.svg",
-  },
-  {
-    label: "Appearance",
-    icon: "./icons/vector-10.svg",
-  },
-  {
-    label: "Plugins",
-    icon: "./icons/vector-11.svg",
-  },
-];
+import { NAV_MENU_ITEMS } from "@/lib/constants";
 
 const NavMenu = () => {
   const [activeTab, setActiveTab] = useState("Home");
--- lib/constants.ts
+++ lib/constants.ts
@@ -58,3 +58,50 @@ export const TABLE_ROWS: TableRowProps[] = [
     total: 9312,
   },
 ];
+
+export const NAV_MENU_ITEMS = [
+  {
+    label: "Home",
+    icon: "./icons/Vector-1.svg",
+  },
+  {
+    label: "Orders",
+    icon: "./icons/Vector-2.svg",
+  },
+  {
+    label: "Products",
+    icon: "./icons/Vector-3.svg",
+  },
+  {
+    label: "Delivery",
+    icon: "./icons/Vector-4.svg",
+  },
+  {
+    label: "Marketing",
+    icon: "./icons/Vector-5.svg",
+  },
+  {
+    label: "Analytics",
+    icon: "./icons/Vector-6.svg",
+  },
+  {
+    label: "Payouts",
+    icon: "./icons/Vector-7.svg",
+  },
+  {
+    label: "Discounts",
+    icon: "./icons/Vector-8.svg",
+  },
+  {
+    label: "Audience",
+    icon: "./icons/Vector-9.svg",
+  },
+  {
+    label: "Appearance",
+    icon: "./icons/Vector-10.svg",
+  },
+  {
+    label: "Plugins",
+    icon: "./icons/Vector-11.svg",
+  },
+];
